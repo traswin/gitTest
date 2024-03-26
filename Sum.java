a8edec2779ee60a44e3b4074a6945ca4063ce653
traswin <traswin.m@gmail.com>
2024-03-26 20:31:12 +0700

    t

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of the numbers from 1 to 100 is " + sum);
+    }
+}
+    
