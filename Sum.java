23e774b126e63d9207497cd212a718fc776e38f1
traswin <traswin.m@gmail.com>
2024-03-26 21:28:47 +0700

    traswin3

--- Sum.java
+++ Sum.java
@@ -9,6 +9,7 @@ public class Sum {
         System.out.println("The sum of the numbers from 1 to 100 is " + sum);
         System.out.println("Traswin");
         System.out.println("Traswin");
+        System.out.println("Traswin");
         
     }
 }
